4b6ed507d02cd699b9c0e9c1b1b5ad14f7f40191
legitbeep <glorytoharsh@gmail.com>
2020-11-24 01:05:23 +0530

    ADD RGB effects

--- gravity.py
+++ gravity.py
@@ -4,54 +4,52 @@ from pygame.locals import *
 from pygame.math import Vector2
 from pygame.draw import *
 from random import randint, random
-screen = None
+from math import e,sin,pi,cos
 
-
+WINSIZE = 1280,720
 
 class Rocket:
-	def __init__(self):
-		self.pos = Vector2(randint(0,WINSIZE[0]), randint(0,WINSIZE[1]))
-		self.vel = Vector2(0,0)
+    def __init__(self):
+        self.pos = Vector2(randint(0,WINSIZE[0]), randint(0,WINSIZE[1]))
+        self.vel = Vector2(0,0)
 
-	def update(self, x, y):
-		
-		temp = Vector2(x, y)
-		acc = temp - self.pos
-		acc = acc.normalize()
-		acc *= 0.05
-		self.vel += acc
-		if self.vel.length() > 5:
-			self.vel = self.vel.normalize()
-			self.vel *= 5
-				
-		self.pos += self.vel
+    def update(self, x, y):
+        
+        temp = Vector2(x, y)
+        acc = temp - self.pos
+        acc = acc.normalize()
+        acc *= 0.05
+        self.vel += acc
+        if self.vel.length() > 5:
+            self.vel = self.vel.normalize()
+            self.vel *= 5
+                
+        self.pos += self.vel
 
-	def show(self, screen, col):
-		circle(screen, col, (int(self.pos.x), int(self.pos.y)), 9)
+    def show(self, screen, col):
+        circle(screen, col, (int(self.pos.x), int(self.pos.y)), 9)
 
 
-WINSIZE = 800, 800
 
 def fade(screen):
-	s = pygame.Surface((WINSIZE[0],WINSIZE[1]))  # the size of your rect
-	s.set_alpha(18)                # alpha level
-	s.fill((0,0,0))           # this fills the entire surface
-	screen.blit(s, (0,0)) 
+    s = pygame.Surface((WINSIZE[0],WINSIZE[1]))  # the size of your rect
+    s.set_alpha(18)                # alpha level
+    s.fill((0,0,0))           # this fills the entire surface
+    screen.blit(s, (0,0)) 
 
 
-def colorFade(c1, base, rev):
-    if c1>254 and rev==False:
-        rev = True
+def colorFade(x, rgb):
+
+    N = x/1000
     
-    if rev==False:
-        c1+=1
-         
-    if rev:
-        c1-=1
-        if c1<base:
-            rev = False
+    if rgb == "r" :
+        x = 255 * abs(sin(N*pi)) 
+    elif rgb == "g" :
+        x = 255 * abs(sin(N*pi+ pi/3)) 
+    elif rgb == "b" :
+        x = 255 * abs(sin(N*pi+2*pi/3)) 
 
-    return c1, rev
+    return int(x)
 
 def main():
     pygame.init()
@@ -64,9 +62,9 @@ def main():
     clock = pygame.time.Clock()
     
     rockets = []
-    N = 500
+    N = 1000
     for i in range(N):
-    	rockets.append(Rocket())
+        rockets.append(Rocket())
 
     done = 0
     c1 = randint(50,255)
@@ -74,22 +72,27 @@ def main():
     c2 = randint(50,255)
     c3 = randint(50,255)
     rev = False
-
+    X = 0
     while not done:
-    	fade(screen)
-    	c1, rev = colorFade(c1, base, rev)
-    	x, y = pygame.mouse.get_pos()
-    	col = c1, c2, c3
-    	for i in range(N):
-    		rockets[i].update(x, y)
-    		rockets[i].show(screen, col)	
-		
-    	for e in pygame.event.get():
-    		if e.type == QUIT:	
-    			done = 1
+        fade(screen)
+        c1 = colorFade(X,"r")
+        c3 = colorFade(X,"b")
+        c2 = colorFade(X,"g")
+        x, y = pygame.mouse.get_pos()
+        col = c1, c2, c3
+        print(col,X)
+        for i in range(N):
+            rockets[i].update(x, y)
+            rockets[i].show(screen, col)    
+        
+        for e in pygame.event.get():
+            if e.type == QUIT:  
+                done = 1
 
-    	pygame.display.update()
-    	clock.tick(60)
+        pygame.display.update()
+        clock.tick(60)
+        X += 1
+        X %= 1000
 
 
 main()
